f7ede6e87fdd77e7449639b5f9250d89c83589ee
NERO2k <nero2k@outlook.com>
2019-11-08 09:19:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ wget 'https://github.com/tesseract-ocr/tessdata/raw/4.00/swe.traineddata'
 sudo mv ./swe.traineddata /usr/share/tesseract-ocr/4.00/tessdata/swe.traineddata
 npm install yarn -g
 mv .env-example .env
+yarn install
 yarn run build
 
 echo Done!
