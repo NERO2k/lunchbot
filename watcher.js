dd9147645cea68eed0f3afe48312105d2c3f9843
NERO2k <nero2k@outlook.com>
2019-11-08 19:15:25 +0100

    Minor fix.

--- watcher.js
+++ watcher.js
@@ -1,6 +1,6 @@
 var spawn = require('child_process').spawn;
 spawn('node', ['--experimental-modules', '--es-module-specifier-resolution=node', 'src/index.js'], {
   cwd: process.cwd(),
-  detached: true,
+  detached: false,
   stdio: "inherit"
 });
