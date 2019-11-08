5e3be9df4175c8e370b9fa9485a129f8c03c9335
NERO2k <nero2k@outlook.com>
2019-11-08 17:13:39 +0100

    Forgot the node args.

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,8 @@ module.exports = {
     "apps": [
         {
             "name": "eatery-bot",
-            "script": "./src/index.js"
+            "script": "./src/index.js",
+			"node_args": ["--experimental-modules", "--es-module-specifier-resolution=node"]
         }
     ]
 }
