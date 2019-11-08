0f39c8908e557020c1c58fcc62d94dbba9401a8e
Casper <pnfcre@gmail.com>
2019-11-08 20:15:18 +0100

    Renaming EateryBot to Lunchbot

--- src/modules/log/index.js
+++ src/modules/log/index.js
@@ -4,11 +4,11 @@ import moment from 'moment'
 import os from 'os'
 
 export default async (type, str, col = '#606060') => {
-  console.log(`${chalk.bgHex('#1e86da')(' EateryBot ') + chalk.bgHex(col)(` ${type} `)} ${str}`)
+  console.log(`${chalk.bgHex('#1e86da')(' Lunchbot ') + chalk.bgHex(col)(` ${type} `)} ${str}`)
 
   fs.appendFile(
     `logs/eatery-bot-${moment().format('YYYY-MM-DD')}.txt`,
-    `${moment().format('HH:mm:ss')} [EateryBot][${type}] ${str}` + os.EOL,
+    `${moment().format('HH:mm:ss')} [Lunchbot][${type}] ${str}` + os.EOL,
     err => {
       if (err) throw err
     }
