6a42131c6ab1495d973d24869e2a6a55c354ac11
NERO2k <nero2k@outlook.com>
2019-11-08 18:55:43 +0100

    Fix pm2 watcher.

--- pm2.config.js
+++ pm2.config.js
@@ -2,8 +2,7 @@ module.exports = {
     "apps": [
         {
             "name": "eatery-bot",
-            "script": "./src/index.js",
-			"node_args": ["--experimental-modules", "--es-module-specifier-resolution=node"]
+            "script": "./watcher.js",
         }
     ]
 }
---
+++ watcher.js
@@ -0,0 +1,6 @@
+var spawn = require('child_process').spawn;
+spawn('node', ['--experimental-modules', '--es-module-specifier-resolution=node', 'src/index.js'], {
+  cwd: process.cwd(),
+  detached: true,
+  stdio: "inherit"
+});
