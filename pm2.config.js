0765faf86efb607e21de6c7a5b403e61d1c5c11d
NERO2k <nero2k@outlook.com>
2020-01-13 18:02:44 +0100

    Fix bot runner.

--- pm2.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: 'lunch-bot',
-      script: './watcher.js'
-    }
-  ]
-}
--- watcher.js
+++
@@ -1,6 +0,0 @@
-var spawn = require('child_process').spawn
-spawn('node', ['--experimental-modules', '--es-module-specifier-resolution=node', 'src/index.js'], {
-  cwd: process.cwd(),
-  detached: false,
-  stdio: 'inherit'
-})
