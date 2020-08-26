ac5f63c11fd20235bd22e41dab9e8893157e4288
NERO2k <nero2k@outlook.com>
2020-08-26 10:38:25 +0200

    Base

--- src/modules/index.js
+++
@@ -1,9 +0,0 @@
-import log from './log'
-import ocr from './ocr'
-import embed from './embed'
-import scrape from './scrape'
-import download from './download'
-import web from './web'
-import format from './format'
-
-export { embed, ocr, log, scrape, download, web, format }
