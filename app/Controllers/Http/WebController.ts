6f71c774469d566e757354ca3d9732f2055d17c9
Seebus <19seth@stockholmscience.se>
2020-08-26 23:10:19 +0200

    something changed

--- app/Controllers/Http/WebController.ts
+++ app/Controllers/Http/WebController.ts
@@ -1,7 +1,22 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import moment from "moment";
+import {Exception} from "@poppinss/utils";
+import {getMenu} from "App/Common/HelperFunctions";
+
 export default class WebController {
-  public async index ({ view }) {
-    return view.render('menu')
+  public async index ({ view, request }) {
+    const params = request.all();
+    const date = moment(params.date, params.format)
+
+    if (!date.isValid())
+      throw new Exception("Date / Date format provided is invalid.")
+
+    let data = await getMenu(date, false)
+    // @ts-ignore
+    data.current_day = data.menu[moment().format("dddd")]
+    // @ts-ignore
+    delete data.menu[moment().format("dddd")]
+    return view.render('menu', data)
   }
 }
