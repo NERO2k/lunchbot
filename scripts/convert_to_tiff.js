33a4ba9f75b0457de127b7159e36d5f292180c7f
NERO2k <nero2k@outlook.com>
2020-09-04 08:58:32 +0200

    Add conversion tools.

--- scripts/convert_to_tiff.js
+++ scripts/convert_to_tiff.js
@@ -1,1 +1,26 @@
-#!/bin/bash
+const {glob} = require("glob")
+const path = require("path")
+const sharp = require("sharp")
+const fs = require("fs/promises")
+
+glob(
+  path.join(__dirname, "../tmp/*.png"),
+  {},
+  async (_err, files) => {
+    for (const path1 of files) {
+      console.log("converting " + path1)
+      let sharpStream = sharp(path1);
+
+      await sharpStream
+        .clone()
+        .tiff()
+        .toFile(path1.replace(".png", ".tif"))
+
+      console.log("converted " + path1)
+
+      await fs.unlink(path1)
+
+      console.log("deleted source image for " + path1)
+    }
+  }
+)
--- scripts/delete_all_ocr.js
+++ scripts/delete_all_ocr.js
@@ -1,1 +1,18 @@
-#!/bin/bash
+const {glob} = require("glob")
+const path = require("path")
+const sharp = require("sharp")
+const fs = require("fs/promises")
+
+glob(
+  path.join(__dirname, "../tmp/*.txt"),
+  {},
+  async (_err, files) => {
+    for (const path1 of files) {
+      console.log("deleting " + path1)
+
+      await fs.unlink(path1)
+
+      console.log("deleted " + path1)
+    }
+  }
+)
