dfb18d06ad803cc1951e2104f40257df60e288e8
NERO2k <nero2k@outlook.com>
2020-09-21 20:23:28 +0200

    Add proper migration.

--- database/migrations/1598433214357_users.ts
+++ database/migrations/1598433214357_users.ts
@@ -4,11 +4,8 @@ export default class Users extends BaseSchema {
   protected tableName = "users";
 
   public async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments("id");
-      table.boolean("enabled");
-      table.string("user_id");
-      table.timestamps(true);
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string("channel_id");
     });
   }
 
