5996f96b4d49ac6f51921bba9f0a8f08624ced46
NERO2k <nero2k@outlook.com>
2020-09-29 21:36:35 +0200

    Remove the thing,

--- app/Common/DiscordHelpers.ts
+++ app/Common/DiscordHelpers.ts
@@ -37,45 +37,27 @@ export async function dispatch(instance, data, date) {
 
   for (const user of users) {
     try {
-      if (user.channel_id) {
-        const userObject = await instance.users.fetch(user.user_id);
-        const userChannel = await instance.channels.fetch(user.channel_id);
-        const messageCollection = await userChannel.messages.fetch({
-          limit: 1,
-        });
-        const latestMessage = messageCollection.first();
-        if (
-          latestMessage.author.id === instance.user.id &&
-          latestMessage.embeds.length > 0 &&
-          latestMessage.embeds[0].title.includes("EATERY") &&
-          latestMessage.embeds[0].title.match(/\d+/g)[0] !== data.actual_week
-        ) {
-          await latestMessage.edit(embedData);
-          Logger.info(
-            `Edited lunch menu in ${user.user_id}, aka ${userObject.username}`
-          );
-        } else {
-          await userChannel.send(embedData);
-          Logger.info(
-            `Sent lunch menu to ${user.user_id}, aka ${userObject.username}`
-          );
-        }
+      const userObject = await instance.users.fetch(user.user_id);
+      const userChannel = await instance.channels.fetch(user.channel_id);
+      const messageCollection = await userChannel.messages.fetch({
+        limit: 1,
+      });
+      const latestMessage = messageCollection.first();
+      if (
+        latestMessage.author.id === instance.user.id &&
+        latestMessage.embeds.length > 0 &&
+        latestMessage.embeds[0].title.includes("EATERY") &&
+        latestMessage.embeds[0].title.match(/\d+/g)[0] !== data.actual_week
+      ) {
+        await latestMessage.edit(embedData);
+        Logger.info(
+          `Edited lunch menu in ${user.user_id}, aka ${userObject.username}`
+        );
       } else {
-        // REMOVE THIS AFTER ONE WEEK
-        const userChannel = await instance.users.fetch(user.user_id);
-        const message = await userChannel.send(embedData);
+        await userChannel.send(embedData);
         Logger.info(
-          `Sent lunch menu to ${user.user_id}, aka ${userChannel.username} using legacy method.`
+          `Sent lunch menu to ${user.user_id}, aka ${userObject.username}`
         );
-        // @ts-ignore
-        await User.updateOrCreate(
-          { user_id: user.user_id },
-          { channel_id: message.channel.id }
-        );
-        Logger.info(
-          `Migrated ${user.user_id}, aka ${userChannel.username} to new system.`
-        );
-        // REMOVE THIS AFTER ONE WEEK
       }
     } catch (error) {
       Logger.error(`Failed to send menu to ${user.user_id}`);
