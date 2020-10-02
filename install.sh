d7716322205963615791b68aa6517d36874f5dda
NERO2k <nero2k@outlook.com>
2020-10-02 20:52:55 +0200

    Add missing scripts and files.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+apt update -y
+apt full-upgrade -y
+apt install -y tesseract-ocr node-gyp make
+curl -O https://github.com/tesseract-ocr/tessdata/raw/master/swe.traineddata
+mv swe.traineddata /usr/share/tesseract-ocr/4.00/tessdata
+
+npm i -g yarn
+cp .env.example .env
+
+yarn install
+
+echo Done!
