b63fc18a3c0c74eaa27af547b3af4001a64955e2
NERO2k <nero2k@outlook.com>
2020-10-14 11:19:48 +0200

    Add websocket test message.

--- start/websocket.ts
+++ start/websocket.ts
@@ -2,13 +2,21 @@ import WebSocket from "ws";
 import Logger from "@ioc:Adonis/Core/Logger";
 import Event from "@ioc:Adonis/Core/Event";
 import Env from "@ioc:Adonis/Core/Env";
+import {getMenu} from "App/Common/HelperFunctions";
+import moment from "moment";
 
 const wss = new WebSocket.Server({ port: <any>Env.get("WS_PORT") });
 
 wss.on('connection', function connection(ws : WebSocket) {
   Logger.info("Client connected to WebSocket.")
-  ws.on('message', function incoming(message) {
+  ws.on('message', async function incoming(message) {
     Logger.info(`Got message "${message}" over websocket.`)
+    if (message === "menu_update_test") {
+      ws.send(JSON.stringify({
+        "event": "menu_update",
+        "message": await getMenu(moment(), false, true)
+      }));
+    }
   });
 });
 
@@ -22,7 +30,7 @@ Event.on("new:menu", async (msg) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify({
         "event": "menu_update",
-        "data": msg.data
+        "message": msg.data
       }));
     }
   });
